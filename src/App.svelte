bd4310ee80830357ea2e5fe2a694f94d1a483bf9
girma-tesfaye <girmatesfaye80@gmail.com>
2024-02-07 02:21:44 -0800

    user input and data binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
-	export let name;
+	let name = "Girma";
+	//let role = "Sr. Dev";
+	let color = "pink";
+	const changeColor = () => {
+		color = "black";
+	}
+	// const inputColor = (event) => {
+	// 	color = event.target.value;
+	// }
+	// const changeRole = () => {
+	// 	role = "Sr. Full Stack dev"
+	// }
+	// const inputRole = (event) => {
+	// 	role = event.target.value;
+	// }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Name: {name}!</h1>
+	<p style="color: {color}">Color: {color}</p>
+	<button style="color: {color}" on:click={changeColor}>Change color</button>
+	<!-- <input type="text" on:input={inputRole} value={role}/> -->
+	<input type="text" style="color: {color}" bind:value={color}/>
 </main>
 
 <style>
